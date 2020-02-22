458c082ae3b969df4675735275c8e282c3e56848
Robert Wongus <rob_wo@yahoo.com>
2020-02-21 23:34:09 -0500

    tried original functions

--- APIQuiz.js
+++ APIQuiz.js
@@ -1,63 +1,86 @@
-let questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
-    },
-    {
-      title:
-        "String values must be enclosed within ____ when being assigned to letiables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    }
-  ];
 
 var currentQuestionIndex = 0;
 let time = questions.length * 20;
 let timerId;
 
   
-  let questions = document.getElementById("questions");
-  var timerEl = document.getElementById("time");
-  let choicesEl = document.getElementById("choices");
-  let submitBtn = document.getElementById("submit");
-  let startBtn = document.getElementById("start");
-  let initialsEl = document.getElementById("initials");
-  let feedbackEl = document.getElementById("feedback");
+  let questions = document.getElementById("#questions");
+  var timerEl = document.getElementById("#time");
+  let choicesEl = document.getElementById("#choices");
+  let submitBtn = document.getElementById("#submit");
+  let letsBegin = document.getElementById("#begin");
+  let initialsEl = document.getElementById("#initials");
+  let feedbackEl = document.getElementById("#feedback");
   
   
 
-startBtn.addEventListener('click', function() {
-    let answers
-  for (i = 0; i < questions.length; i++) {
-        answers = prompt(questions[i].title)
+// letsBegin.addEventListener('click', function() {
+//   for (i = 0; i < questions.length; i++) {
+//     var choicesEl (this.title)   
         
-  }
+//   }
+
+function startQuiz() {
+    letsBegin.classList.add(hide);
+    time = questions.length * 15;
+          
+    }
 
 }
 );
 
+function questionClick() {
+    
+    if (this.value !== questions[currentQuestionIndex].answer) {
+    
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+
+function startQuiz() {
+    
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    
+    questionsEl.removeAttribute("class");
+    
+    timerId = setInterval(clockTick, 1000);
+  
+    timerEl.textContent = time;
+  
+    getQuestion();
+  }
+
+  currentQuestion.choices.forEach(function(choice, i) {
+    
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    
+    choiceNode.onclick = questionClick;
+
+    
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+  
+  function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
 @media screen and (max-width: 500px) {
     .column {
       width: 100%;
